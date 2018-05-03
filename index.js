89b55e3b7afc66d66f0ebfead14371172d7d040f
Sergey <inferpse@users.noreply.github.com>
2018-05-03 17:57:03 +0300

    updated for webpack 4

--- index.js
+++ index.js
@@ -1,28 +1,57 @@
+const { ReplaceSource } = require('webpack-sources');
+
 class JSONPNamespacePlugin {
   apply(compiler) {
-    compiler.plugin('compilation', function(compilation) {
-      compilation.mainTemplate.plugin('bootstrap', function(source) {
-        const jsonpFunction = this.outputOptions.jsonpFunction,
-              defaultTemplate = `window[${JSON.stringify(jsonpFunction)}]`,
-              desiredNamespace = `window.${jsonpFunction}`;
+    compiler.hooks.compilation.tap('JSONPNamespacePlugin', compilation => {
+      const { jsonpFunction } = compilation.outputOptions,
+            defaultTemplate = `window[${JSON.stringify(jsonpFunction)}]`,
+            desiredNamespace = `window.${jsonpFunction}`;
 
-        // generate code which will initialize namespace
-        let initializeNS = jsonpFunction.split('.').map((item, index, array) => {
-          const prop = array.slice(0, index + 1).join('.');
-          return index < array.length - 1 ? `window.${prop} = window.${prop} || {};` : ``;
-        }).filter(str => str.length).join('\n');
+      // do nothing if "jsonpFunction" does not contain "."
+      if (jsonpFunction.indexOf('.') < 0) {
+        return;
+      }
 
+      // introduce namespace in the beginning of bootstrap section (if not introduced yet)
+      compilation.mainTemplate.hooks.bootstrap.tap('JSONPNamespacePlugin', source => {
         if (source.indexOf('webpackJsonpCallback') < 0) {
           // do not introduce namespace for entry points without dynamic chunk loading
           return source;
         } else {
+          // generate code which will initialize namespace
+          let initializeNS = jsonpFunction.split('.').map((item, index, array) => {
+            const prop = array.slice(0, index + 1).join('.');
+            return index < array.length - 1 ? `window.${prop} = window.${prop} || {};` : ``;
+          }).filter(str => str.length).join('\n');
+
           // prepend namespace initialization code and replace original template parts
-          return this.asString([
+          return [
             initializeNS,
             source.split(defaultTemplate).join(desiredNamespace)
-          ]);
+          ].join('')
         }
       });
+
+      // replace usage of JSONP function inside the mainTemplate
+      compilation.mainTemplate.hooks.beforeStartup.tap('JSONPNamespacePlugin', source => {
+        return source.split(defaultTemplate).join(desiredNamespace);
+      });
+
+      // update chunk template as well
+      compilation.chunkTemplate.hooks.render.tap('JSONPNamespacePlugin', source => {
+        const origTemplate = `(window[${JSON.stringify(jsonpFunction)}] = window[${JSON.stringify(jsonpFunction)}] || []).push(`,
+              desiredTemplate = `(window.${jsonpFunction} = window.${jsonpFunction} || []).push(`,
+              matchIndex = source.source().indexOf(origTemplate);
+
+        // if chunk contains expected template
+        if (matchIndex > -1) {
+          source = new ReplaceSource(source);
+          source.replace(matchIndex, matchIndex + origTemplate.length - 1, desiredTemplate);
+        }
+
+        return source;
+      });
+
     });
   }
 }
