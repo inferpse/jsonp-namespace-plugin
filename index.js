e32621cedba73ed61c75bb2072ccc34271b654d6
inferpse <inferpse@gmail.com>
2020-12-04 19:26:49 +0200

    webpack 5 compatibility

--- index.js
+++ index.js
@@ -1,59 +1,68 @@
+const webpack = require('webpack');
+const { RuntimeModule } = webpack;
+const { JavascriptModulesPlugin } = webpack.javascript;
 const { ReplaceSource } = require('webpack-sources');
 
 class JSONPNamespacePlugin {
   apply(compiler) {
+    compiler.hooks.thisCompilation.tap('JSONPNamespacePlugin', compilation => {
+      if (!isPluginRequired(compilation)) return;
+      compilation.hooks.additionalTreeRuntimeRequirements.tap('JSONPNamespacePlugin', (chunk, set) => {
+        if (!isEnabledForChunk(compilation, chunk)) return;
+        compilation.addRuntimeModule(chunk, new JSONPNamespaceRuntimeModule());
+      });
+    });
+
     compiler.hooks.compilation.tap('JSONPNamespacePlugin', compilation => {
-      const { jsonpFunction } = compilation.outputOptions,
-            defaultTemplate = `window[${JSON.stringify(jsonpFunction)}]`,
-            desiredNamespace = `window.${jsonpFunction}`;
+      if (!isPluginRequired(compilation)) return;
 
-      // do nothing if "jsonpFunction" does not contain "."
-      if (jsonpFunction.indexOf('.') < 0) {
-        return;
-      }
-
-      // introduce namespace in the beginning of bootstrap section (if not introduced yet)
-      compilation.mainTemplate.hooks.bootstrap.tap('JSONPNamespacePlugin', source => {
-        if (source.indexOf('webpackJsonpCallback') < 0) {
-          // do not introduce namespace for entry points without dynamic chunk loading
-          return source;
-        } else {
-          // generate code which will initialize namespace
-          let initializeNS = jsonpFunction.split('.').map((item, index, array) => {
-            const prop = array.slice(0, index + 1).join('.');
-            return index < array.length - 1 ? `window.${prop} = window.${prop} || {};` : ``;
-          }).filter(str => str.length).join('\n');
-
-          // prepend namespace initialization code and replace original template parts
-          return [
-            initializeNS,
-            source.split(defaultTemplate).join(desiredNamespace)
-          ].join('')
-        }
+      JavascriptModulesPlugin.getCompilationHooks(compilation).renderMain.tap('JSONPNamespacePlugin', source => {
+        return replaceTemplate(compilation, source);
       });
 
-      // replace usage of JSONP function inside the mainTemplate
-      compilation.mainTemplate.hooks.beforeStartup.tap('JSONPNamespacePlugin', source => {
-        return source.split(defaultTemplate).join(desiredNamespace);
+      JavascriptModulesPlugin.getCompilationHooks(compilation).renderChunk.tap('JSONPNamespacePlugin', source => {
+        return replaceTemplate(compilation, source);
       });
-
-      // update chunk template as well
-      compilation.chunkTemplate.hooks.render.tap('JSONPNamespacePlugin', source => {
-        const origTemplate = `(window[${JSON.stringify(jsonpFunction)}] = window[${JSON.stringify(jsonpFunction)}] || []).push(`,
-              desiredTemplate = `(window.${jsonpFunction} = window.${jsonpFunction} || []).push(`,
-              matchIndex = source.source().indexOf(origTemplate);
-
-        // if chunk contains expected template
-        if (matchIndex > -1) {
-          source = new ReplaceSource(source);
-          source.replace(matchIndex, matchIndex + origTemplate.length - 1, desiredTemplate);
-        }
-
-        return source;
-      });
-
     });
   }
+}
+
+class JSONPNamespaceRuntimeModule extends RuntimeModule {
+  constructor() {
+    super('jsonp namespace plugin init');
+  }
+  generate() {
+    const { chunkLoadingGlobal } = this.compilation.outputOptions;
+    return chunkLoadingGlobal.split('.').map((item, index, array) => {
+      const prop = array.slice(0, index + 1).join('.');
+      return index < array.length - 1 ? `self.${prop} = self.${prop} || {};` : ``;
+    }).filter(str => str.length).join('\n');
+  }
+}
+
+const isPluginRequired = (compilation) => {
+  const { chunkLoadingGlobal } = compilation.outputOptions;
+  return chunkLoadingGlobal.includes('.');
+}
+
+const isEnabledForChunk = (compilation, chunk) => {
+  const globalChunkLoading = compilation.outputOptions.chunkLoading;
+  const options = chunk.getEntryOptions();
+  const chunkLoading = (options && options.chunkLoading) || globalChunkLoading;
+  return chunkLoading === 'jsonp';
+}
+
+const replaceTemplate = (compilation, source) => {
+  const { chunkLoadingGlobal } = compilation.outputOptions;
+  const expectedTemplate = `self[${JSON.stringify(chunkLoadingGlobal)}] = self[${JSON.stringify(chunkLoadingGlobal)}] || []`;
+  const desiredTemplate = `self.${chunkLoadingGlobal} = self.${chunkLoadingGlobal} || []`;
+
+  const matchIndex = source.source().indexOf(expectedTemplate);
+  if (matchIndex > -1) {
+    source = new ReplaceSource(source);
+    source.replace(matchIndex, matchIndex + expectedTemplate.length - 1, desiredTemplate);
+  }
+  return source;
 }
 
 module.exports = JSONPNamespacePlugin;
